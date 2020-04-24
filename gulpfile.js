204f6bb9087a2db534d6ec04848f90ba7739ab03
Masolg <44949062+Masolg@users.noreply.github.com>
2020-04-24 18:28:14 +0200

    Commited gulp-app

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const { src, dest } = require('gulp');
+const less = require('gulp-less');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+
+function css() {
+    return src('less/**/*.less')
+    .pipe(less())
+    .pipe(dest('css'))
+    .pipe(browserSync.stream())
+}
+
+function image() {
+    return src('src/images/*')
+    .pipe(imagemin())
+    .pipe(dest('dist/images'))
+}
+
+function watch(){
+  browserSync.init({
+    server: {
+      baseDir: './',
+    }
+  });
+  gulp.watch('./src/images/*', image);
+  gulp.watch('./less/**/*.less', css);
+  gulp.watch('./*.html').on('change', browserSync.reload)
+}
+
+
+exports.watch = watch;
